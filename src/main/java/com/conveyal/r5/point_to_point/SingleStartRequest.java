9899f883ff9b960a0aeca452a84a1d51bda8932e
norbert frese <norbert.frese@prisms-solutions.com>
2025-04-21 16:20:32 +0200

    MAX_PATH_DESTINATIONS = 15_000 monteCarloDraws = 5

--- src/main/java/com/conveyal/r5/point_to_point/SingleStartRequest.java
+++ src/main/java/com/conveyal/r5/point_to_point/SingleStartRequest.java
@@ -119,7 +119,7 @@ public class SingleStartRequest {
 		task.date = LocalDate.of(2025,2,5);
 		task.fromTime = 7 * 60 * 60;
 		task.maxTripDurationMinutes = 30;
-		task.monteCarloDraws = 10;
+		task.monteCarloDraws = 5;
 		task.toTime = 9 * 60 * 60;
 		task.transitModes = EnumSet.of(TransitModes.BUS, 
 				TransitModes.RAIL, 
